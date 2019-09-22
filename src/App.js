ce5e893e245dccf9b9d93106ded46391af5de809
Polymorphy12 <46921525+Polymorphy12@users.noreply.github.com>
2019-09-22 23:06:39 +0900

    2019-09-22

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import 'semantic-ui-css/semantic.min.css'
+// import Header from './header/Header';
+// import Footer from './footer/Footer';
+// import RestaurantCard from './restaurantCard/RestaurantCard';
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <RestaurantCard />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import RestaurantContainer from './restaurantList/restaurantContainer';
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import RestaurantCard from './restaurantCard/RestaurantCard';
 
 function App() {
   return (
     <div>
-      <Header />
-      <RestaurantCard />
-      <Footer />
+      <Header/>
+      <RestaurantContainer/>
+      <Footer/>
     </div>
   );
 }
 
 export default App;
+
